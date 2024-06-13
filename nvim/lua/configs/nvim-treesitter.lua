4c8e209291c6edcc7dbbab0927189a64dafa153c
TheKenkei <nowikow.witaliy@gmail.com>
2024-06-14 01:07:48 +0300

    feat: update config for `nvim-treesiter`

--- nvim/lua/configs/nvim-treesitter.lua
+++ nvim/lua/configs/nvim-treesitter.lua
@@ -10,5 +10,10 @@ local options = {
     "javascript",
     "php",
   },
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
 }
 return options
